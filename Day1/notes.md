dad371300f9deea3974371e1fa53d4bcfe1a8958
Dharungandhy28 <dharungandhi28@gmail.com>
2022-12-26 20:40:33 +0530

    main

---
+++ Day1/notes.md
@@ -0,0 +1,22 @@
+### This is Javascript 15 days basic code practice:
+
+### Day 1
+
+1. Introduction
+2. Basic data types
+
+Intro:
+
+### variables :
+
+1. It is a name given for a memory location
+2. It is always be userdefined
+
+syntax: var variablename
+
+### Data types:
+
+1. There are some basic data types like string, number, boolean
+2. In the use of comparision data type then boolean is automatically works
+3. concatinating opeartion can be performed if we want to add the string data type
+   number data type and boolean data type
---
+++ Day1/script.js
@@ -0,0 +1,76 @@
+// 1. Introduction
+
+
+// Declaring variable in different data types
+
+var name = "Dharun" // string
+var age = 18 // number
+var arr = true // Boolean
+
+console.log("Hello all");
+
+// declaring variables with number
+
+const pie = 3.14 // pie value
+
+// declaring variable in one line by using comma
+
+let name1 = 'varun',
+job = 'It',
+live = 'Finland'
+
+console.log(name1);
+
+
+// 2. Data Types 
+
+
+// Declaring number
+
+var year = 2022
+var month = 12
+
+// Declaring string 
+ 
+var abc = "Vimal"
+let obj = "hello"
+
+// concatinating string data type using addition operation
+
+let space = ' ';
+var com = "DharunGandhi is a "
+var salary = 100000
+var role = "Software developer and his salary is "
+
+var full = com + space + role + space + salary
+console.log(full);
+
+
+// Declaring boolean
+
+var joe = true
+var don = false
+
+// Declaring comparisons
+
+// Triple ===
+
+const num = 0;
+const obj1 = new String("0");
+const str = "0";
+
+console.log(num === num); // true
+console.log(obj === obj); // true
+console.log(str === str); // true
+
+console.log(num === obj); // false
+console.log(num === str); // false
+console.log(obj === str); // false
+
+// Double ==
+
+const num1 = 0;
+const big = 0n;
+console.log(big == num1); // true
+
+
